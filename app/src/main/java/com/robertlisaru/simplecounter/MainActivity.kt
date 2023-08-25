95159f60ba8f2244e14962538a8507d6d5ce476f
Lisaru Robert <lisaru.robert@gmail.com>
2023-08-25 16:38:16 +0300

    Add increment logic

--- app/src/main/java/com/robertlisaru/simplecounter/MainActivity.kt
+++ app/src/main/java/com/robertlisaru/simplecounter/MainActivity.kt
@@ -2,10 +2,21 @@ package com.robertlisaru.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var counter: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counterValue = 0
+
+        counter = findViewById(R.id.counterText)
+        counter.setOnClickListener {
+            counterValue++
+            counter.text = counterValue.toString()
+        }
     }
 }
